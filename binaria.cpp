932a66d19a2b5d02148696f4d22c1a8117fb046b
gpaccip <gpaccip@unjbg.edu.pe>
2024-09-27 15:21:35 -0500

    binaria con bublesort

--- binaria.cpp
+++ binaria.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int binaria(int a[], int n, int dato); 
+void bublesort(int x[], int n);
 
 int main(){
 	int n, a[100], dato, pos;
@@ -12,11 +13,26 @@ int main(){
 		cin>>a[i];
 	}
 	cout<<"ingrese el dato que quiere buscar: "; cin>>dato;
+	bublesort(a, n);
 	pos=binaria(a, n, dato);
 	cout<<"el numero esta en la posicion: "<<pos;
 	return 0;
 }
-
+void bublesort(int x[], int n){
+	int aux;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-i; j++){
+			if(x[j]>x[j+1]){
+				aux=x[j];
+				x[j]=x[j+1];
+				x[j+1]=aux;
+			}
+		}
+	}
+	for(int i=0; i<n; i++){
+		cout<< x[i];
+	}
+}
 int binaria(int a[], int n, int dato){
 	int izq=0, der=n-1, cen=0, m, pos;
 	while(izq<=der and cen==0){
